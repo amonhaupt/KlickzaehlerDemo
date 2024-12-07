f23daa9450f9172bcda3c4a6419da82c29dbe519
amonhaupt <amon.haupt@aol.com>
2024-12-07 14:11:49 +0100

    fixed ClickCounter collisionShape

--- V1.3/index.service.worker.js
+++ V1.3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733085963|8831946662';
+const CACHE_VERSION = '1733577018|11181617428';
 /** @type {string} */
 const CACHE_PREFIX = 'Klickzaehler-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
